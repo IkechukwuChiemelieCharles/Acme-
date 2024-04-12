ac8b0a301c0fed763b2da40ab1f90bc41535cdb3
Ikechukwu Chiemelie <chiemeliecharleschristopher@gmail.com>
2024-04-12 14:58:51 -0700

    added side bar with js

--- build/JS/main.js
+++ build/JS/main.js
@@ -1,12 +1,33 @@
-const initApp = () => {
-  const hamburgerBtn = document.getElementById("harmburger-button");
-  const mobileMenu = document.getElementById("mobile-menu");
+// const initApp = () => {
+//   const hamburgerBtn = document.getElementById("harmburger-button");
+//   const mobileMenu = document.getElementById("mobile-menu");
 
-  const toggleMenu = () => {
-    mobileMenu.classList.toggle("hidden");
-    mobileMenu.classList.toggle("flex");
-  };
-  hamburgerBtn.addEventListener("click", toggleMenu);
-  mobileMenu.addEventListener("click", toggleMenu);
+//   const toggleMenu = () => {
+//     mobileMenu.classList.toggle("hidden");
+//     mobileMenu.classList.toggle("flex");
+//   };
+//   hamburgerBtn.addEventListener("click", toggleMenu);
+//   mobileMenu.addEventListener("click", toggleMenu);
+// };
+// document.addEventListener("DOMcontentLoaded", initApp);
+
+const sideBar = document.querySelector("#mobilemenu");
+const harmburgerbutton = document.querySelector("#harmburgerbutton");
+
+const toggleMenu = () => {
+  sideBar.classList.toggle("hidden");
+  sideBar.classList.toggle("flex");
 };
-document.addEventListener("DOMcontentLoaded", initApp);
+harmburgerbutton.addEventListener("click", toggleMenu);
+sideBar.addEventListener("click", toggleMenu);
+// harmburgerbutton.addEventListener("click", () => {
+//   sideBar.classList.toggle("hidden");
+// });
+
+///////////////////////// working
+// const sideBar = document.querySelector("#mobilemenu");
+// const harmburgerbutton = document.querySelector("#harmburgerbutton");
+
+// harmburgerbutton.addEventListener("click",  () => {
+//   sideBar.classList.toggle("hidden");
+// });
